c167d52370ad1ba1ee45389cc093f5b074f422ce
giorochi <rochiragiovanni87@gmail.com>
2026-05-03 17:17:22 +0200

    Change import statement to use App.jsx extension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Importa il file App.jsx che hai già
+import App from './App.jsx'; // Importa il file App.jsx che hai già
 
 // Collega React al div con id="root" che abbiamo creato nell'HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
