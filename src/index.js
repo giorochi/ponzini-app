6dc09fc27553a1e98273a7f49e30fca3f6ea89ca
giorochi <rochiragiovanni87@gmail.com>
2026-05-03 17:13:42 +0200

    Add comments to index.js for better understanding
    Added comments in Italian to index.js for clarity.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // Importa il file App.jsx che hai già
 
+// Collega React al div con id="root" che abbiamo creato nell'HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
